7659dcdb4ec392b22e9be2a18852891db9d13c84
Yegor Sawinkov <mubest1701@gmail.com>
2025-01-16 14:35:51 +0200

    Add adaptive + slider

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalBtnOpen = document.querySelector('.modal__btn--open');
+const modalBtnClose = document.querySelector('.modal__btn--close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
