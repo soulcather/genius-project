5f3b2eef1ab72bc451cc6828b9f0c38ecb86f42d
Yegor Sawinkov <mubest1701@gmail.com>
2025-01-18 22:18:34 +0200

    Fix modal window

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,12 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal__btn--open');
 const modalBtnClose = document.querySelector('.modal__btn--close');
+const modalBtnHeroSectionOpen = document.querySelector(
+  '.modal__btn-hero-section--open',
+);
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnHeroSectionOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
